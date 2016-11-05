f2ae4c441174f5e9d3c9b8494c53dcd5da08e63d
Jose Praveen <JosePraveen@users.noreply.github.com>
2016-11-05 22:40:06 +0530

    Create Product.java

---
+++ RESTDBLab1/src/com/josetechblog/model/Product.java
@@ -0,0 +1,42 @@
+package com.josetechblog.model;
+
+public class Product {
+
+	private String productCode;
+	private String productName;
+	private String productLine;
+	private String productScale;
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductLine() {
+		return productLine;
+	}
+
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+
+	public String getProductScale() {
+		return productScale;
+	}
+
+	public void setProductScale(String productScale) {
+		this.productScale = productScale;
+	}
+
+}
