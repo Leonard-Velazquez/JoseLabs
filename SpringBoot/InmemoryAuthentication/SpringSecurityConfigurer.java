63b5dfaa1e1c0b211fb964b6b544380e9523a6a9
Jose Praveen <JosePraveen@users.noreply.github.com>
2016-11-14 23:11:43 +0530

    Create SpringSecurityConfigurer.java

---
+++ SpringBoot/InmemoryAuthentication/SpringSecurityConfigurer.java
@@ -0,0 +1,20 @@
+package com.josetechblog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("jose").password("jose1234").roles("USER").and().withUser("admin")
+				.password("admin").roles("USER", "ADMIN");
+	}
+
+}
